1f29407cca3e6f8c2c0caf90bf680f6a09c4694d
wilmat9322 <wilmat9322@br.uipr.edu>
2023-01-16 11:06:04 -0500

    final commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -74,7 +74,7 @@ const Hero = (props) => {
                         }}/></Text>
               
                 <BtnLink
-                    href="https://drive.google.com/file/d/1Acj9rtF5XIfIMxIZSVj0Z0_fwArmDQe8/view?usp=share_link"
+                    href="https://drive.google.com/file/d/1hT4rYuUguIUaesRVL5AXEBMGPube2c1K/view?usp=share_link"
                     target="_blank">
                     <Button>
                     {button1} 
