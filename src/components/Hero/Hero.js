ec98c3342f10c303f055601b5043fef450eaf36b
wilmat9322 <prisa15.matos@gmail.com>
2023-03-30 21:28:48 -0400

    resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -74,7 +74,7 @@ const Hero = (props) => {
                         }}/></Text>
               
                 <BtnLink
-                    href="https://drive.google.com/file/d/1Jkfy6ASv9RROwIln3srnfHO2GHHIByWp/view?usp=share_link"
+                    href="https://drive.google.com/file/d/1DT9yPbYqk_2jvFUsy7GjX0arND7yxwQ1/view?usp=share_link"
                     target="_blank">
                     <Button>
                     {button1} 
